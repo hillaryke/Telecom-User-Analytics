c42f06e146930610ca3ac84233e24eef371e3e7f
Hillary Kipkemoi <tenskap@gmail.com>
2024-04-24 07:10:22 +0300

    fix query text clause error

--- src/database_connection.py
+++ src/database_connection.py
@@ -6,7 +6,7 @@ class DatabaseHandler:
         self.db_config = db_config
         self.engine = create_engine(f"postgresql://{db_config['username']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}")
 
-    def get_data_from_db(self, query):
+    def get_data_from_db(self, query: str):
         query = text(query)
         with self.engine.connect() as conn:
             df = pd.read_sql_query(query, conn)
--- tests/test_database_connection.py
+++ tests/test_database_connection.py
@@ -2,16 +2,16 @@ import unittest
 import pandas as pd
 from src.database_connection import DatabaseHandler
 from src.config import db_config
-from sqlalchemy import text
+from src.utils import fetch_data_from_db
 
 class TestDatabaseHandler(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.db_handler = DatabaseHandler(db_config)
 
-    def test_get_data_from_db(self):
+    def test_fetch_data_from_db(self):
         query = "SELECT * FROM xdr_data"
-        df = self.db_handler.get_data_from_db(query)
+        df = fetch_data_from_db(query)
 
         # Check if the function returns a DataFrame
         self.assertIsInstance(df, pd.DataFrame)

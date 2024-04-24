969f276a39fdad6220783883c79d96afeeaedbe4
Hillary Kipkemoi <tenskap@gmail.com>
2024-04-24 07:10:22 +0300

    fix query text clause error

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,5 @@ from src.config import db_config
 
 def fetch_data_from_db(query):
     db_handler = DatabaseHandler(db_config)
-    query = text(query)
     df = db_handler.get_data_from_db(query)
     return df

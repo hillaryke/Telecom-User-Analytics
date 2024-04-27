0f13aa2a77d140628888d184af400b447d320040
Hillary Kipkemoi <tenskap@gmail.com>
2024-04-27 11:48:02 +0300

    update pandas to resolve deprecation warnings of np

--- tests/test_process_data.py
+++ tests/test_process_data.py
@@ -1,12 +1,9 @@
 import unittest
-import warnings
 from src.preprocessing import preprocess_data
 from src.utils import fetch_telecom_data
 
 class TestPreprocessData(unittest.TestCase):
     def test_unique_columns(self):
-        # Ignore the DeprecationWarning
-        # warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # Load the data
         df = fetch_telecom_data()
